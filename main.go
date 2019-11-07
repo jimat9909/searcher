fb0d1c2ca0b4ebd8a8d3f2e2d773b262fdd3876f
jimat9909 <50683610+jimat9909@users.noreply.github.com>
2019-11-07 16:27:06 -0600

    update main.go
    Add cli commands
    Fix concurrency bug returning results early

--- main.go
+++ main.go
@@ -5,12 +5,11 @@ import (
 	"fmt"
 	"os"
 	"sync"
+	"strconv"
 	"strings"
 	"sort"
 	"net/url"
 	"github.com/jimat9909/processurl"
-	
-	
 )
 // Global Index of terms 
 
@@ -142,16 +141,14 @@ func CrawlURL (url string, token chan struct{})  processurl.UrlParseResults{
 	return theseResults
 }
 
-func Crawl (rooturl string, depth, concurrency int, visited VisitedMap, index Index, titles URLtitles)  crawlSummary {
-
-	// fmt.Printf("Crawl: Starting to retrieve root %v Depth %v\n", rooturl, depth)
+var crawlwg sync.WaitGroup 
+func Crawl (rooturl string, maxdepth, concurrency int, visited VisitedMap, index Index, titles URLtitles)  crawlSummary {
 	
 	parsedrooturl, _ := url.Parse(rooturl)
 	rootHost := strings.TrimPrefix(parsedrooturl.Host, "www.")
-	//fmt.Printf("Crawl: rooturl scheme %v host %v\n", parsedrooturl.Scheme, rootHost)
 	
-	
-	requestlist := make(chan []crawlRequest)
+	// need to make this a buffered channel so we can have multiple request sets 
+	requestlist := make(chan []crawlRequest, 1000)
 	tokens := make( chan struct{}, concurrency)
 	
 	uniquePages := 0
@@ -160,10 +157,15 @@ func Crawl (rooturl string, depth, concurrency int, visited VisitedMap, index In
 	go func() {requestlist <- []crawlRequest{rootRequest} }()
 	var n int
 	n++
+	
 	for ; n > 0; n-- {
 		
 		fmt.Printf(".")
 		requests := <-requestlist
+		
+		// wait for all these requests to process 
+		// to make sure all work is done before returning results
+		crawlwg.Add(len(requests))
 		for _, request := range requests {
 			
 			// Clean up the requested url a little
@@ -173,23 +175,37 @@ func Crawl (rooturl string, depth, concurrency int, visited VisitedMap, index In
 			cleanRequestURL = strings.TrimPrefix(cleanRequestURL, "www.")
 			cleanRequestURL = strings.TrimSuffix(cleanRequestURL, "/")
 
-			// Only check the Host, not the Scheme
-			// Add in the returned depth for re-crawling at a "higher" level
-			_, ok := visited.Value(cleanRequestURL)
-			// fmt.Printf("CRAWL: Looking at request.url %v, clean %v, OK %v\n", request.url, cleanRequestURL, ok)
+			
+			// See if we need to visit this URL.  Don't include the scheme in the check
+			
+			doIndexing := true
+			priorDepth, ok := visited.Value(cleanRequestURL)
+			
+			// check to see if the url has already been scanned.  If it had been scanned a deeper level
+			// rescan it but don's add to the index any terms. This gets into delete / deep delete issues  
+			// we want to rescan in case there are more links that we should scan
+			if ok && request.depth < priorDepth {
+				ok = false
+				doIndexing = false
+				// fmt.Printf("Crawl: rescanning %v prior depth: %v request depth %v\n", cleanRequestURL, priorDepth, request.depth)
+			}
+			
 			if !ok {
 				visited.Visit(cleanRequestURL, request.depth)
-				if request.depth < 2 {
+				if request.depth < maxdepth {
 					n++
 				}
 				uniquePages++
-				go func(request crawlRequest, token chan struct{}) {
+				go func(request crawlRequest, doIndexing bool, token chan struct{}) {
+					defer crawlwg.Done()
 					theseResults := CrawlURL(request.url, token)
-					_, unique := index.Add(theseResults.URL, theseResults.Index) 
-					//fmt.Printf("URL %v had  %v were unique\n", theseResults.URL, unique)
-					titles.Add(theseResults.URL, theseResults.Title)
-					uniqueTerms += unique
-					if request.depth < 2 {
+					if doIndexing {
+						_, unique := index.Add(theseResults.URL, theseResults.Index) 
+						uniqueTerms += unique
+						titles.Add(theseResults.URL, theseResults.Title)
+					}
+					
+					if request.depth < maxdepth {
 						newrequestlist := []crawlRequest{}
 						for newurl, _ := range theseResults.EmbeddedURL {
 							parsednewurl, err := url.Parse(newurl)
@@ -197,14 +213,11 @@ func Crawl (rooturl string, depth, concurrency int, visited VisitedMap, index In
 								continue
 							}
 							newHost := strings.TrimPrefix(parsednewurl.Host, "www.")
-							if newHost == rootHost {
+							if newHost == rootHost || CrawlForeign {
 								newrequest := crawlRequest{newurl, request.depth + 1}
 								newrequestlist = append(newrequestlist, newrequest)
 							}
-							
-							
 						} // end going through the embedded urls
-						// fmt.Printf("Crawl gofunc: Adding %v requests to list\n", len(newrequestlist))
 
 						if len(newrequestlist) > 0 {
 							requestlist <- newrequestlist
@@ -212,10 +225,14 @@ func Crawl (rooturl string, depth, concurrency int, visited VisitedMap, index In
 							n--
 						}
 					} // end adding more work
-				
-				}(request, tokens)
-			} 
+				}(request, doIndexing, tokens)
+			} else {
+				// not going to do this url, so mark it complete
+				crawlwg.Done()
+			}
 		} // end going through this set of requests
+		// wait for each request to complete
+		crawlwg.Wait()
 		
 	}
 	// fmt.Printf("****** Crawl: Finally done with crawling root: Pages %v Terms %v\n", uniquePages, uniqueTerms)
@@ -223,11 +240,17 @@ func Crawl (rooturl string, depth, concurrency int, visited VisitedMap, index In
 	return crawlSummary{uniquePages, uniqueTerms}
 }
 
+// config variables
+
+var version = "1.0.0"
+var CrawlForeign = false
+var Concurrency = 10
+var MaxDepth = 2
 
 
 func main() {
 
-	
+	fmt.Printf("Searcher %v initializing\n", version)
 	// Set up our main data structures 
 	index := Index{entries: make(map[string][]IndexEntry)}
 	visited := VisitedMap{v: make(map[string]int)}
@@ -263,6 +286,10 @@ func main() {
 		
 			case "clear": 
 				Reset(visited, index, titles)
+			case "config": 
+				ShowConfig()
+			case "set": 
+				Set(command[1])
 			case "quit", "q": 
 				break	cliLoop
 				
@@ -294,13 +321,15 @@ func GetURL (rooturl string, visited VisitedMap, index Index, titles URLtitles) 
 	
 	
 	fmt.Printf("Initiating crawl of %v \n", rooturl)
-	results := Crawl(rooturl, 0, 10, visited, index, titles)
+	results := Crawl(rooturl, MaxDepth, Concurrency, visited, index, titles)
 	fmt.Printf("Indexed %v pages and %v terms\n\n", results.uniquePages, results.uniqueTerms)
 	return
 }
 
 func DisplayTerm (term string, index Index, titles URLtitles) {
-	
+	if !processurl.CaseSensitive {
+		term = strings.ToLower(term)
+	}
 	termList := index.GetTerm(term) 
 	if termList == nil {
 		fmt.Printf("Search term \"%v\" not found\n\n", term)
@@ -332,7 +361,86 @@ func Help() {
 	fmt.Printf("\t index (url) \tThis will search and index the specified url and the links\n")
 	fmt.Printf("\t search (term) \tThis will return the pages' URLS, titles and count that contain the search term\n")
 	fmt.Printf("\t clear \tThis will reset the index\n")
+	fmt.Printf("\t config \tThis will show configuration settings\n")
 	fmt.Printf("\t quit \tThis will quit the program\n")
+	fmt.Printf("\n\t set (argument) \t\tset the configuration variable accordingly. Arguments are:\n")
+	fmt.Printf("\t\tcase | nocase\tdefine case sensitivity for terms.  nocase means terms will be converted to lowercase prior to saving in the index\n")
+	fmt.Printf("\t\tindexanchors | noindexanchors\tdefines whether or to index the title attribute on an anchor tag\n")
+	fmt.Printf("\t\tcrawlforeign | nocrawlforeign\tdefines whether or to crawl links pointed to hosts outside the root domain\n")
+	fmt.Printf("\t\tconcurrency (integer) Number of concurrent crawls.  Must be 1 or more\n")
+	fmt.Printf("\t\tdepth (integer) Number of levels to crawl, the root url being level 1.  Must be 1 or more\n")
+
+	
+
+}
+
+func ShowConfig() {
+	fmt.Printf("Configuration settings:\n")
+	fmt.Printf("\tCase Sensitive %v\tIf false, convert terms to lower case before indexing\n", processurl.CaseSensitive)
+	fmt.Printf("\tIndex Anchors %v\tIf true, index the titles of anchor tags\n", processurl.IndexAnchorTitles)
+	fmt.Printf("\tCrawl Foreign %v\tIf true, crawl links to URLs outside of the root URL domain\n", CrawlForeign)
+	fmt.Printf("\tMaximum Depth %v\t\tHow many levels of embedded links to crawl\n", MaxDepth + 1)
+	fmt.Printf("\tConcurrency %v\t\tHow many concurrent pages to crawl\n", Concurrency)
+	
+}
+
+func Set(command string) {
+
+	commandArgs := strings.SplitN(command, " ", 2)
+	switch commandArgs[0] {
+		case "case": 
+			processurl.CaseSensitive = true
+			fmt.Printf("Indexing is now case sensitive\n")
+		case "nocase":
+			processurl.CaseSensitive = false
+			fmt.Printf("Indexing is now case insensitive\n")
+		case "indexanchors":
+			processurl.IndexAnchorTitles = true
+			fmt.Printf("Anchor titles will be indexed\n")
+		case "noindexAnchors":
+			processurl.IndexAnchorTitles = false
+			fmt.Printf("Anchor titles will not be indexed\n")
+		case "crawlforeign":
+			CrawlForeign = true
+			fmt.Printf("Links outside of the root domain will be searched\n")
+		case "nocrawlforeign":
+			CrawlForeign = false
+			fmt.Printf("Links outside of the root domain will not be searched\n")
+		case "concurrency": 
+			i, err := strconv.Atoi(commandArgs[1])
+			if err != nil {
+				fmt.Printf("%v not integer: %v\n", commandArgs[1], err)
+				break
+			}
+			
+			if i < 1 {
+				fmt.Printf("Concurrency must be greater than 0\n")
+				break
+			}
+			Concurrency = i;
+			fmt.Printf("Concurrency set to %v\n", Concurrency)
+			
+		case "depth": 
+			i, err := strconv.Atoi(commandArgs[1])
+			if err != nil {
+				fmt.Printf("%v not integer: %v\n", commandArgs[1], err)
+					break
+				}
+					
+				if i < 1 {
+					fmt.Printf("Depth must be greater than 0\n")
+					break
+				}
+			MaxDepth = i - 1
+			fmt.Printf("Depth set to %v\n", MaxDepth + 1)
+			
+			
+					
+		default:
+			Help()
+					
+		}
+
 
 }
 
